d6d78fe451e91c8095fda1fe7a69c1e3300ef5f8
nguewouo mabkop manick <nguewouom@gmail.com>
2023-03-13 05:22:09 +0100

    test backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./header/Header";
+import TestApi from "./testApi/TestApi";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <TestApi></TestApi>
     </div>
   );
 }
---
+++ src/testApi/TestApi.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class TestApi extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8000/api/posts')
+      .then(response => response.json())
+      .then(data => this.setState({ posts: data }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Posts</h1>
+        <ul>
+          {this.state.posts.map(post => (
+            <li key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.content}</p>
+              <h2>{post.user.name}</h2>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TestApi;
